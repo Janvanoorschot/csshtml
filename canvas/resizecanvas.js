1828064b73937755e9eb0d4ef0000865eb2e02e2
Jan van Oorschot <janvanoorschot@gmail.com>
2021-10-10 11:58:43 +0200

    Got canvas the size of the div.

---
+++ canvas/resizecanvas.js
@@ -0,0 +1,17 @@
+
+function resizecanvas_ready() {
+    resize_canvas_paint();
+    $(window).resize(resizecanvas_resize);
+}
+
+function resizecanvas_resize() {
+    resize_canvas_paint();
+}
+
+function resize_canvas_paint() {
+    let canvas = $('#thecanvas')[0];
+    var ctx=canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(95,50,40,0,2*Math.PI);
+    ctx.stroke();
+ }
